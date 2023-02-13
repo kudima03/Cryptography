f993054bf74aa97e4a7d80ea09b559127a11b401
Dmitry Kurochkin <93078951+kudima03@users.noreply.github.com>
2023-02-13 12:56:39 +0300

    Unnecessary usings removed.

--- Benchmarks/KeyPhraseBenchmark.cs
+++ Benchmarks/KeyPhraseBenchmark.cs
@@ -1,10 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Cryptography.KeyPhraseEncryption;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Benchmarks
 {
